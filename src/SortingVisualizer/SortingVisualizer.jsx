ad4c3ec2aa14237266e96144417de84fce7d4678
sethudi <stmapunya@gmail.com>
2021-09-22 18:26:06 +0200

    Created

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -76,11 +76,3 @@ export default class SortingVisualizer extends React.Component{
 function randomIntFromIntervals(min,max) {
     return Math.floor(Math.random() *(max-min +1) + min);
 }
-function arraysAreEqual(arrayOne, arrayTwo) {
-    if (arrayOne.length !== arrayTwo.length) return false;
-
-    for (let i=0; i<arrayOne.length; i++){
-        if (arrayOne[i] !== arrayTwo[i]) return false;
-    }
-    return true;
-}
